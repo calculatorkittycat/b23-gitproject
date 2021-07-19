4a6719e4547af2a2203bdf98d1a9c628490a0112
calculatorkittycat <cicain90@gmail.com>
2021-07-19 13:44:57 -0700

    test

--- src/Cool.java
+++ src/Cool.java
@@ -6,7 +6,7 @@ public class Cool {
         int[] lol = {1, 2, 3, 4, 5, 6};
         System.out.println("Cool");
 
-        for (int i = lol.length; i < 0; i--);//ggg {
+        for (int i = lol.length; i < 0; i--);//gggg {
 
         }
     }
